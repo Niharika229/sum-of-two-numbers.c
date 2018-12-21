42029aa3297a087615785912236bb25986e19e9e
Niharika229 <46022706+Niharika229@users.noreply.github.com>
2018-12-21 17:22:07 +0530

    Add files via upload

---
+++ SUM.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum;
+clrscr();
+printf("Enter a value: ");
+scanf("%d",&a);
+printf("Enter b value: ");
+scanf("%d",&b);
+sum=a+b;
+printf("Total:%d",sum);
+getch();
+}
